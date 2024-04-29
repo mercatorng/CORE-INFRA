237851be76c9cea332d72b1ec1fdb5a97d7395be
oghene-kevwe <idisepraise@gmail.com>
2024-04-29 19:48:38 +0100

    reafactor active page functionalities
    Signed-off-by: oghene-kevwe <idisepraise@gmail.com>

--- src/utils/context.jsx
+++ src/utils/context.jsx
@@ -1,16 +1,33 @@
 import React, { useContext, useReducer } from "react";
 import { appReducer } from "./reducer";
 import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
 
 export const AppContext = React.createContext();
 
 const defaultState = {
   activePage: "",
 };
+
 export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(appReducer, defaultState);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const path = window.location.pathname;
+  const [activeLink, setActiveLink] = useState(window.location.pathname);
+
+  // set persistent page title for solution pages on refresh
+  const pageTitle = sessionStorage.getItem("pageTitle");
+
+  useEffect(() => {
+    // set page title to default title for home about and contact page on refresh
+    // else grab page title from session storage for persistent page title
+    if (path === "/" || path === "/about" || path === "/contact-us") {
+      document.title =
+        "CoreInfra - Modern payments infrastructure for Financial institutions";
+    } else {
+      document.title = pageTitle;
+    }
+  }, [path]);
 
   useEffect(() => {
     // Add an event listener to update the window width when it changes
@@ -25,9 +42,22 @@ export const AppProvider = ({ children }) => {
     setWindowWidth(window.innerWidth);
   };
 
+  // check contact us path
+  const contactUsPath = "/contact-us";
+  const checkContactUsPath = path === contactUsPath;
+
   return (
     <AppContext.Provider
-      value={{ ...state, dispatch, windowWidth, }}
+      value={{
+        ...state,
+        dispatch,
+        windowWidth,
+        path,
+        activeLink,
+        setActiveLink,
+        checkContactUsPath,
+        contactUsPath
+      }}
     >
       {children}
     </AppContext.Provider>
